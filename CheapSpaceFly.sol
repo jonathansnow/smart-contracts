4274fa8b7bb52692c1090fd02837c45e306a550b
Jonathan Snow <jonathan.snow@bellaliant.net>
2021-12-04 10:08:00 -0400

    Suggestions

--- CheapSpaceFly.sol
+++ CheapSpaceFly.sol
@@ -5,10 +5,12 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
 contract CheapSpaceFly is ERC721, Ownable {
   using Strings for uint256;
+  using SafeMath for uint256;
 
   using Counters for Counters.Counter;
   Counters.Counter private _tokenSupply;
@@ -18,13 +20,20 @@ contract CheapSpaceFly is ERC721, Ownable {
   uint256 public maxSupply = 10000;
   uint256 public maxMintAmount = 1;
   uint256 public nftPerAddressLimit = 1;
-  bool public salePaused = true;
-  bool public onlyAllowlisted = true;
-  address[] public allowlistedAddresses;
+
+  bool public saleActive = false;
+  bool public whitelistActive = false;
+
+  mapping(address => bool) public whitelist;
   mapping(address => uint256) public addressMintedBalance;
 
-  constructor() ERC721("CheapSpaceFly", "SSF") {
+  constructor(address[] memory addresses) ERC721("CheapSpaceFly", "SSF") {
     setBaseURI("ipfs://QmQxTmCi2sXecN4wi4KQKWcfUXesAFnGHKNUGDE4oi5t1e/");
+
+    // Set up initial whitelist
+    for (uint256 i = 0; i < addresses.length; i++) {
+        updateWhitelist(addresses[i]);
+    }
   }
 
   // internal
@@ -32,37 +41,42 @@ contract CheapSpaceFly is ERC721, Ownable {
     return baseURI;
   }
 
-  // public
+  // Public sale mint function
   function mint(address _to, uint256 _mintAmount) public payable {
-    require(!salePaused, "the contract is paused");
-    uint256 supply = _tokenSupply.current();
-    require(_mintAmount > 0, "need to mint at least 1 NFT");
+    require(saleActive, "the contract is paused");
+    require(_mintAmount > 0, "need to mint at least 1 NFT"); // May be overkill, unlikely and not a problem if 0
     require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
-    require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+    require(_tokenSupply.current() + _mintAmount <= maxSupply, "max NFT limit exceeded");
 
-    if (msg.sender != owner()) {
-        if(onlyAllowlisted == true) {
-            require(isAllowlisted(msg.sender), "user is not whitelisted");
-            uint256 ownerMintedCount = addressMintedBalance[msg.sender];
-            require(ownerMintedCount + _mintAmount <= nftPerAddressLimit, "max NFT per address exceeded");
-        }
-        require(msg.value >= cost * _mintAmount, "insufficient funds");
-    }
+    uint256 ownerMintedCount = addressMintedBalance[msg.sender];
+    require(ownerMintedCount + _mintAmount <= nftPerAddressLimit, "max NFT per address exceeded");
+    require(msg.value >= cost.mul(_mintAmount), "insufficient funds");
 
-    for (uint256 i = 1; i <= _mintAmount; i++) {
+    for (uint256 i = 0; i < _mintAmount; i++) {
       addressMintedBalance[msg.sender]++;
       _tokenSupply.increment();
-      _safeMint(_to, supply + i);
+      _safeMint(_to, _tokenSupply.current());
     }
   }
 
-  function isAllowlisted(address _user) public view returns (bool) {
-    for (uint i = 0; i < allowlistedAddresses.length; i++) {
-      if (allowlistedAddresses[i] == _user) {
-          return true;
-      }
+  // Public whitelist mint function
+  function whitelistMint(address _to, uint256 _mintAmount) public payable {
+    require(whitelistActive, "whitelist mint not active");
+    require(isWhitelisted(msg.sender), "no whitelist tokens to mint");
+    require(_mintAmount > 0, "need to mint at least 1 NFT"); // May be overkill, unlikely and not a problem if 0
+    require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
+    require(_tokenSupply.current() + _mintAmount <= maxSupply, "max NFT limit exceeded");
+    require(msg.value >= cost.mul(_mintAmount), "insufficient funds");
+
+    uint256 ownerMintedCount = addressMintedBalance[msg.sender];
+    require(ownerMintedCount + _mintAmount <= nftPerAddressLimit, "max NFT per address exceeded");
+    require(msg.value >= cost.mul(_mintAmount), "insufficient funds");
+
+    for (uint256 i = 0; i < _mintAmount; i++) {
+      addressMintedBalance[msg.sender]++;
+      _tokenSupply.increment();
+      _safeMint(_to, _tokenSupply.current());
     }
-    return false;
   }
 
   function tokenURI(uint256 tokenId)
@@ -83,7 +97,33 @@ contract CheapSpaceFly is ERC721, Ownable {
         : "";
   }
 
+  // Function to return the number of NFTs an address is whitelisted for
+  function isWhitelisted(address userAddress) public view returns (bool)  {
+      return whitelist[userAddress];
+  } 
+
+  // Function to display the total supply from Counter
+  function totalSupply() public view returns (uint256) {
+      return _tokenSupply.current();
+  }
+
   //only owner
+
+  // Function to alllow the contract owner to mint NFTs
+  function ownerMint(address _to, uint256 _mintAmount) public onlyOwner {
+    require(_tokenSupply.current() + _mintAmount <= maxSupply, "max NFT limit exceeded");
+    // Add in additional owner mint logic here if needed
+
+    for (uint256 i = 0; i < _mintAmount; i++) {
+      _tokenSupply.increment();
+      _safeMint(_to, _tokenSupply.current());
+    }
+  }
+
+  // Function to whitelist a user
+  function updateWhitelist(address userAddress) public onlyOwner {
+    whitelist[userAddress] = true;
+  }
   
   function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
     nftPerAddressLimit = _limit;
@@ -102,16 +142,11 @@ contract CheapSpaceFly is ERC721, Ownable {
   }
   
   function toggleSale() public onlyOwner {
-    salePaused = !salePaused;
+    saleActive = !saleActive;
   }
   
-  function setOnlyAllowlisted(bool _state) public onlyOwner {
-    onlyAllowlisted = _state;
-  }
-  
-  function allowlistUsers(address[] calldata _users) public onlyOwner {
-    delete allowlistedAddresses;
-    allowlistedAddresses = _users;
+  function toggleWhitelist() public onlyOwner {
+    whitelistActive = !whitelistActive;
   }
  
   function withdrawBalance() public onlyOwner {
